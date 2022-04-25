ef8537917ec0c02890a0bc9676e8ea12f540054d
Luca Kiebel <github@luca-kiebel.de>
2022-04-25 08:52:39 +0200

    Add Access Token Verification function

--- config.example.ts
+++ config.example.ts
@@ -1,5 +1,8 @@
 const config = {
   PORT: 3000,
+  MONGO_URL: 'mongodb://localhost:27017/test',
+  JWT_PUBLIC_KEY_ADDRESS:
+    'https://auth-service.dev.example.com/auth/public-key',
 };
 
 export default config;
