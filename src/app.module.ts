cfede0a06067014427954b230fed2962782121b7
Luca Kiebel <github@luca-kiebel.de>
2022-05-03 13:02:40 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { KeycloakModule } from './keycloak/keycloak.module';
+import { KeycloakModule } from '@nibyou/keycloak';
 
 @Module({
   imports: [KeycloakModule],
