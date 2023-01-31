ba80d29539232635583d26ea0c4184dff75e6034
Luca Kiebel <github@luca-kiebel.de>
2023-01-31 12:45:24 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import 'dotenv/config';
+import { json } from 'body-parser';
+import * as morgan from 'morgan';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 (async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  const corsOptionsCallback = (req, callback) => {
-    const corsOptions = { origin: false, credentials: true };
-    if (process.env.CORS_ORIGINS.split(',').includes(req.headers.origin)) {
-      corsOptions.origin = true;
-    }
-    callback(null, corsOptions);
-  };
-  
-  app.enableCors(corsOptionsCallback)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors();
+  app.use(json({ limit: '100mb' }));
+  app.set('trust proxy', true);
+  app.use(morgan('short'));
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Nibyou Microservice')
